67cec53347b209a4c6f7d09f8ad7fd128ff12967
antonio martinez baez <tono.mtzb@gmail.com>
2020-08-05 17:08:41 -0500

    finished routing

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,55 @@
 const express = require('express');
 
+const postsDb = require('./postDb')
+
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  postsDb.get()
+    .then( posts =>{
+      res.status(200).json(posts)
+    })
+    .catch( err =>{
+      res.status(500).json({message: 'something went wrong'})
+    })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId, (req, res) => {
+  postsDb.getById(req.params.id)
+    .then( post =>{
+      res.status(200).json(post)
+    })
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validatePostId, (req, res) => {
+    postsDb.remove(req.params.id)
+      .then( post =>{
+        res.status(200).json({message: 'Post was sucessfully deleted'})
+      })
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePostId, (req, res) => {
+  postsDb.update(req.params.id, req.body)
+  .then( count =>{
+    if( count === 1){
+      res.status(200).json({message: "post updated successfully"})
+    } else {
+      res.status(500).json({ error: "Something went wrong" })
+    }
+  })
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  postsDb.getById(req.params.id)
+    .then(post => {
+      if(post){
+        next()
+      } else {
+        res.status(404).json({message: 'The post id does not exist'})
+      }
+    })
 }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,25 @@ const express = require('express');
 
 const server = express();
 
+//routes
+const userRouter = require('./users/userRouter')
+const postsRouter = require('./posts/postRouter')
+
+//middleware functions
 const logger = require('./middleware/logger')
 
-server.use(logger)
 
+
+server.use(express.json())
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
+server.use('/user', userRouter);
+
+server.use('/posts', postsRouter)
 //custom middleware
 
 
